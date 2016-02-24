d332f65a446d0a410fae46190767894e753b5833
Yunus Gaziev <yunus.gaziev@gmail.com>
2016-02-24 14:16:24 +0500

    debounce для js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('alert');
+// Возвращает функцию, которая не будет срабатывать, пока продолжает вызываться.
+// Она сработает только один раз через N миллисекунд после последнего вызова.
+// Если ей передан аргумент `immediate`, то она будет вызвана один раз сразу после
+// первого запуска.
+function debounce(func, wait, immediate) {
+
+    var timeout;
+
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
+    };
+};
