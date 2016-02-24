1964f81bfaba90726346f571676e79491a75eb8d
Yunus Gaziev <yunus.gaziev@gmail.com>
2016-02-24 14:18:59 +0500

    Функция определения версии IE 10 или 11

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,3 +21,24 @@ function debounce(func, wait, immediate) {
         if (callNow) func.apply(context, args);
     };
 };
+
+/**
+ * определение версии IE 10 или 11
+ *
+ * @returns {Number} 10, 11, или 0 если это не IE
+ */
+function GetIEVersion() {
+  var sAgent = window.navigator.userAgent;
+  var Idx = sAgent.indexOf("MSIE");
+
+  // If IE, return version number.
+  if (Idx > 0)
+    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+  // If IE 11 then look for Updated user agent string.
+  else if (!!navigator.userAgent.match(/Trident\/7\./))
+    return 11;
+
+  else
+    return 0; //It is not IE
+}
