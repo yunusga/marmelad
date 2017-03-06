38c14b111450cf83df8f05d500310318265244be
yunusga <yunusga@yandex.ru>
2017-03-06 18:22:54 +0500

    Прощайте, старый хлам

--- assets/_helpers/concat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function() {
-
-    let args = Array.prototype.slice.call(arguments, 0);
-    args.pop();
-
-    return args.join('').toString();
-}
--- assets/marmelad.data.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    app      : {
-        lang      : 'ru',
-        themeColor: '#000',
-        package   : 'ключ перезаписывается значениями из package.json marmelad-сборщика',
-        settings  : 'ключ перезаписывается значениями из файла настроек marmelad.settings.js',
-    },
-    pageTitle: 'MARMELAD'
-};
