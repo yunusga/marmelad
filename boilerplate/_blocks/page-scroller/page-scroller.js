00d98ed75060469afd1245badb9c1fff5425925d
yunusga <yunusga@yandex.ru>
2017-05-01 16:21:49 +0500

    Добавил блок скроллера страницы в заготовку

---
+++ boilerplate/_blocks/page-scroller/page-scroller.js
@@ -0,0 +1,62 @@
+;(function($) {
+
+    'use strict';
+
+    var page          = $('html, body'),
+        pageScroller  = $('.m-page-scroller'),
+        pageYOffset   = 0,
+        inMemory      = false,
+        inMemoryClass = 'is-memorized';
+
+    function resetPageScroller() {
+
+        if (!inMemory) {
+
+            pageYOffset = 0;
+            pageScroller.removeClass(inMemoryClass);
+        }
+
+        inMemory = false;
+    }
+
+    if (pageScroller.length > 0) {
+
+        window.addEventListener('scroll', resetPageScroller, false);
+
+        if (window.outerWidth < $('body').prop('scrollHeight')) {
+            pageScroller.addClass('is-visible');
+        }
+
+        pageScroller.on('click', function(event) {
+
+            event.preventDefault();
+
+            window.removeEventListener('scroll', resetPageScroller);
+
+            if (window.pageYOffset > 0 && pageYOffset === 0) {
+
+                inMemory = true;
+                pageYOffset = window.pageYOffset;
+
+                pageScroller.addClass(inMemoryClass);
+
+                page.stop().animate({ scrollTop : 0 }, 500, 'swing', function() {
+
+                    window.addEventListener('scroll', resetPageScroller, false);
+                });
+
+            } else {
+
+                pageScroller.removeClass(inMemoryClass);
+
+                page.stop().animate({ scrollTop : pageYOffset }, 500, 'swing', function() {
+
+                    pageYOffset = 0;
+                    window.addEventListener('scroll', resetPageScroller, false);
+                });
+
+            }
+        });
+    }
+
+})(jQuery);
