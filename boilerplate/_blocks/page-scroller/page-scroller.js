0bbf3035ca88fb9b9e71eb4c69acbba5c1efd313
yunusga <yunusga@yandex.ru>
2017-05-05 19:45:41 +0500

    Доработка page-scroller в заготовке

--- boilerplate/_blocks/page-scroller/page-scroller.js
+++ boilerplate/_blocks/page-scroller/page-scroller.js
@@ -6,9 +6,20 @@
         pageScroller  = $('.m-page-scroller'),
         pageYOffset   = 0,
         inMemory      = false,
-        inMemoryClass = 'is-memorized';
+        inMemoryClass = 'is-memorized',
+        enabledOffset = 60;
 
     function resetPageScroller() {
+
+        setTimeout(function() {
+
+            if (window.pageYOffset > enabledOffset) {
+                pageScroller.addClass('is-visible');
+            } else if (!pageScroller.hasClass(inMemoryClass)) {
+                pageScroller.removeClass('is-visible');
+            }
+
+        }, 150);
 
         if (!inMemory) {
 
@@ -22,10 +33,6 @@
     if (pageScroller.length > 0) {
 
         window.addEventListener('scroll', resetPageScroller, false);
-
-        if (window.outerWidth < $('body').prop('scrollHeight')) {
-            pageScroller.addClass('is-visible');
-        }
 
         pageScroller.on('click', function(event) {
 
@@ -41,7 +48,6 @@
                 pageScroller.addClass(inMemoryClass);
 
                 page.stop().animate({ scrollTop : 0 }, 500, 'swing', function() {
-
                     window.addEventListener('scroll', resetPageScroller, false);
                 });
 
