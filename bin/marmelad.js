ce988cc640d4e64a0c30e1689fda0e43ae1bf2d2
yunusga <yunusga@yandex.ru>
2017-05-07 11:08:38 +0500

    Хелпер create переехал в исполняемый файл
    Подключение зависимостей в хелпере оказалось невозможным

--- bin/marmelad.js
+++ bin/marmelad.js
@@ -90,6 +90,25 @@ let getHbsPartialsPaths = (blocksPath) => {
  * https://www.npmjs.com/package/handlebars-layouts
  */
 compileHandlebars.Handlebars.registerHelper(hbsLayouts(compileHandlebars.Handlebars));
+compileHandlebars.Handlebars.registerHelper('create', function(blockName) {
+
+    const settings = require(path.join(process.cwd(), 'marmelad', 'settings.marmelad'));
+
+    let pathToBlockDir = path.join(settings.paths._blocks, blockName);
+    let pathToBlockFile = path.join(pathToBlockDir, blockName);
+    let tpl_hbs = `<div block="m-${blockName}">${blockName} content</div><!-- b:m-${blockName} -->`;
+    let tpl_styl = `.m-${blockName} {}`;
+
+    if (!fs.existsSync(pathToBlockDir)) {
+
+        fs.ensureDirSync(pathToBlockDir);
+        fs.writeFileSync(`${pathToBlockFile}.hbs`, tpl_hbs, 'utf-8');
+        fs.writeFileSync(`${pathToBlockFile}.styl`, tpl_styl, 'utf-8');
+        fs.writeFileSync(`${pathToBlockFile}.js`, '', 'utf-8');
+
+        console.log(`\n[${gutil.colors.green('CREATE BLOCK')}] ${blockName} ${gutil.colors.green('successfully')}\n`);
+    }
+});
 
 /**
  * обновление данных для шаблонов
