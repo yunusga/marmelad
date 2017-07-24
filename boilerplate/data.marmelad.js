eecfe8b54f5fe32c31dd1aa4a371667497be560e
yunusga <yunusga@yandex.ru>
2017-07-24 15:54:12 +0500

    Допиливание заготовки под проект

--- boilerplate/data.marmelad.js
+++ boilerplate/data.marmelad.js
@@ -8,18 +8,6 @@ module.exports = {
         settings  : 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
         storage   : 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
         buildTime : '',
-        icons     : [
-            "menu",
-            "chevron-left",
-            "chevron-down",
-            "chevron-up",
-            "chevron-right",
-            "close",
-            "more_horiz",
-            "more_vert",
-            "favorite",
-            "favorite_border"
-        ],
         controls : [
             'default',
             'brand',
