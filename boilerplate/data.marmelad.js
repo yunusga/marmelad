f7e4b9875949cb23b5e98d519d27825a5762cba3
yunusga <yunusga@yandex.ru>
2017-07-28 16:54:50 +0500

    Google Analytics

--- boilerplate/data.marmelad.js
+++ boilerplate/data.marmelad.js
@@ -4,6 +4,7 @@ module.exports = {
         stylus    : {
             theme_color : '#3E50B4'
         },
+        GA        : false, // Google Analytics site's ID
         package   : 'ключ перезаписывается значениями из package.json marmelad-сборщика',
         settings  : 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
         storage   : 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
