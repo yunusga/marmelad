f7d53def2ee1c9e0bcd24cafac7c88e5525f3cbe
yunusga <yunusga@yandex.ru>
2017-08-25 10:26:30 +0500

    Сделал модуль форматирования HTML самомстоятельным

--- modules/gulp-pretty/index.js
+++ modules/gulp-pretty/index.js
@@ -1,8 +1,49 @@
 'use strict';
 
-const gutil = require('gulp-util');
-const through = require('through2');
-const pretty = require('pretty');
+const gutil    = require('gulp-util');
+const through  = require('through2');
+const beautify = require('js-beautify');
+const condense = require('condense-newlines');
+
+
+function ocd(str, opts) {
+
+    // Normalize and condense all newlines
+    return condense(str, opts)
+        // Remove empty whitespace the top of a file.
+        .replace(/^\s+/g, '')
+        // Remove extra whitespace from eof
+        .replace(/\s+$/g, '\n')
+
+        // Add a space above each comment
+        .replace(/(\s*<!--)/g, '\n$1')
+        // Bring closing comments up to the same line as closing tag.
+        .replace(/>(\s*)(?=<!--\s*\/)/g, '> ');
+}
+
+function pretty(str, opts) {
+
+    opts = Object.assign({
+        unformatted: ['code', 'pre', 'em', 'strong', 'span'],
+        indent_inner_html: true,
+        indent_char: ' ',
+        indent_size: 2,
+        sep: '\n'
+    }, opts);
+  
+    str = beautify.html(str, opts);
+
+    if (opts.ocd === true) {
+
+        if (opts.newlines) {
+            opts.sep = opts.newlines;
+        }
+        
+        return ocd(str, opts);
+    }
+
+    return str;
+};
 
 module.exports = function (opts) {
 
