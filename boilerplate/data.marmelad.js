5d15e6a1667df0ac4da70c02d1591cc6fe589e52
Yunus Gaziev <yunusga@yandex.ru>
2018-11-27 16:10:08 +0500

    💨 кодстайл

--- boilerplate/data.marmelad.js
+++ boilerplate/data.marmelad.js
@@ -1,22 +1,22 @@
 module.exports = {
-    app      : {
-        lang      : 'ru',
-        stylus    : {
-            theme_color : '#3E50B4'
-        },
-        GA        : false, // Google Analytics site's ID
-        package   : 'ключ перезаписывается значениями из package.json marmelad-сборщика',
-        settings  : 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
-        storage   : 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
-        buildTime : '',
-        controls : [
-            'default',
-            'brand',
-            'success',
-            'info',
-            'warning',
-            'danger'
-        ]
+  app: {
+    lang: 'ru',
+    stylus: {
+      theme_color: '#3E50B4',
     },
-    pageTitle: 'marmelad'
+    GA: false, // Google Analytics site's ID
+    package: 'ключ перезаписывается значениями из package.json marmelad-сборщика',
+    settings: 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
+    storage: 'ключ перезаписывается значениями из файла настроек settings.marmelad.js',
+    buildTime: '',
+    controls: [
+      'default',
+      'brand',
+      'success',
+      'info',
+      'warning',
+      'danger',
+    ],
+  },
+  pageTitle: 'marmelad',
 };
