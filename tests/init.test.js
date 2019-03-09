a93edbbfee85ed622d4a54b806b84e4feeaaee11
Yunus Gaziev <yunusga@yandex.ru>
2019-03-09 13:24:31 +0500

    Изменил пути запуска для тестирования релизных версий и хотфиксов

--- tests/init.test.js
+++ tests/init.test.js
@@ -5,7 +5,7 @@ FSE.removeSync('test/init-test');
 FSE.ensureDirSync('test/init-test');
 
 test('init in new directory', (done) => {
-  CMD.exec('mmd init test/init-test', (err, res) => {
+  CMD.exec('node bin/marmelad.js init test/init-test', (err, res) => {
     expect(err).toBe(null);
     expect(res.message).toBe('[marmelad] initialized, type marmelad -h for CLI help\n');
     done();
@@ -13,7 +13,7 @@ test('init in new directory', (done) => {
 });
 
 test('init with create new directory', (done) => {
-  CMD.exec('mmd init test/init-test/create', (err, res) => {
+  CMD.exec('node bin/marmelad.js init test/init-test/create', (err, res) => {
     expect(err).toBe(null);
     expect(res.message).toBe('[marmelad] initialized, type marmelad -h for CLI help\n');
     done();
@@ -21,7 +21,7 @@ test('init with create new directory', (done) => {
 });
 
 test('init in initialized directory', (done) => {
-  CMD.exec('mmd init test/init-test', (err, res) => {
+  CMD.exec('node bin/marmelad.js init test/init-test', (err, res) => {
     expect(err).toBe(null);
     expect(res.message).toBe('[error] project is already initialized\n');
     done();
@@ -32,7 +32,7 @@ test('init in non empty directory', (done) => {
   FSE.ensureDirSync('test/init-test/nonempty');
   FSE.ensureFileSync('test/init-test/nonempty/nonempty.txt');
 
-  CMD.exec('mmd init test/init-test/nonempty --test', (err, res) => {
+  CMD.exec('node bin/marmelad.js init test/init-test/nonempty --test', (err, res) => {
     expect(err).toBe(null);
     expect(res.message).toBe('[warn] Directory is not empty. Some files may be overwritten. Continue?\n');
     done();
