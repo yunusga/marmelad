9a5f4171c1570f2405fc5cb51eb6cbf3d62a9af4
Yunus Gaziev <yunusga@yandex.ru>
2019-12-24 17:21:03 +0500

    dist и hasher для новой команды

---
+++ commands/dist.js
@@ -0,0 +1,60 @@
+const gulp = require('gulp');
+const postHTML = require('gulp-posthtml');
+const attrsSorter = require('posthtml-attrs-sorter');
+const pretty = require('gulp-pretty-html');
+const ora = require('ora');
+
+const hasher = require('../modules/posthtml/hasher');
+
+module.exports = () => {
+  const settings = require(`${process.cwd()}/marmelad/settings.marmelad`);
+
+  const htmlFmtOpts = {
+    html: {
+      indent_size: 4,
+      indent_char: '',
+      indent_with_tabs: true,
+      wrap_line_length: 0,
+      preserve_newlines: false,
+      unformatted: ['code', 'pre', 'em', 'strong', 'span', 'i', 'b', 'br', 'symbol'],
+    },
+    ...settings.pretty,
+  };
+
+  gulp.task('format:html', (done) => {
+    const htmlSpinner = ora('Format HTML started').start();
+
+    gulp.src(`${settings.paths.dist}/**/*.html`)
+      .pipe(pretty(htmlFmtOpts.html))
+      .pipe(gulp.dest(settings.paths.dist));
+
+    htmlSpinner.succeed('Format HTML done');
+
+    done();
+  });
+
+  gulp.task('hasher', (done) => {
+    gulp.src(`${settings.paths.dist}/**/*.html`)
+      .pipe(postHTML([
+        attrsSorter({
+          order: [
+            'id', 'class', 'name',
+            'data-.+', 'ng-.+', 'src',
+            'for', 'type', 'href',
+            'values', 'title', 'alt',
+            'role', 'aria-.+',
+            '$unknown$',
+          ],
+        }),
+        hasher({
+          attributes: [],
+          path: settings.paths.dist,
+        }),
+      ]))
+      .pipe(gulp.dest(settings.paths.dist));
+
+    done();
+  });
+
+  gulp.series('format:html', 'hasher')();
+};
---
+++ modules/posthtml/hasher/index.js
@@ -0,0 +1,63 @@
+const hasha = require('hasha');
+const isUrl = require('is-url');
+const queryString = require('query-string');
+const normalizeUrl = require('normalize-url');
+
+function setFileHash(url, path) {
+  const fullUrl = /^[/?]/.test(url) ? `foo.bar${url}` : url;
+
+  if (!isUrl(normalizeUrl(fullUrl))) {
+    return url;
+  }
+
+  let [uri, query] = fullUrl.split('?');
+
+  const hash = hasha.fromFileSync(`${path}/${uri}`, { algorithm: 'sha1', encoding: 'hex' });
+
+  query = queryString.parse(query);
+  query.v = hash;
+  query = queryString.stringify(query);
+
+  return `${uri}?${query}`;
+}
+
+module.exports = (options = {}) => (tree) => new Promise((resolve, reject) => {
+  let tags = ['link', 'script'];
+  let attributes = ['href', 'src'];
+
+  if (options.tags && Array.isArray(options.tags)) {
+    tags = [...new Set([...tags, ...options.tags])];
+  }
+
+  if (options.exclude) {
+    tags = tags.filter((tag) => !options.exclude.includes(tag));
+  }
+
+  if (options.attributes && Array.isArray(options.attributes)) {
+    attributes = [...new Set([...attributes, ...options.attributes])];
+  }
+
+  if (!Array.isArray(tree)) {
+    reject(new Error('tree is not Array'));
+  }
+
+  if (tree.length === 0) {
+    resolve(tree);
+  }
+
+  tree.walk((node) => {
+    if (node.tag && node.attrs) {
+      node.attrs = Object.keys(node.attrs).reduce((attributeList, attr) => {
+        if (tags.includes(node.tag) && attributes.includes(attr)) {
+          return Object.assign(attributeList, { [attr]: setFileHash(node.attrs[attr], options.path) });
+        }
+
+        return attributeList;
+      }, node.attrs);
+    }
+
+    return node;
+  });
+
+  resolve(tree);
+});
