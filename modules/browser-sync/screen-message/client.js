360c8c422590408f4a2467ef3377d4c80e7189bd
Yunus Gaziev <yunusga@yandex.ru>
2020-09-03 18:48:05 +0500

    Улучшены стили окна ошибки, + копка закрытия окна ошибки

--- modules/browser-sync/screen-message/client.js
+++ modules/browser-sync/screen-message/client.js
@@ -1,4 +1,4 @@
-(function(socket) {
+((socket) => {
   function isInPage(node) {
     return (node === document.body) ? false : document.body.contains(node);
   }
@@ -7,6 +7,8 @@
   const TEMPLATE = `
     <style>
       .__bs_scrMsg {
+        font-family: monospace;
+        font-weight: 400;
         position: fixed;
         z-index: 100500;
         top:0;
@@ -18,6 +20,21 @@
         padding: 16px;
         font-size:16px;
         line-height: 1.45;
+      }
+      .__bs_scrMsg__descr {
+        position: relative;
+      }
+      .__bs_scrMsg__close {
+        position: absolute;
+        padding: 0;
+        top: 0;
+        right: 0;
+      }
+      .__bs_scrMsg__close_icon {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        vertical-align: top;
       }
       .__bs_scrMsg__content {
         background-color: #f6f8fa;
@@ -33,9 +50,12 @@
         overflow: auto;
       }
       </style>
-    <div class="__bs_scrMsg">
+    <div id="__bs_scrMsg" class="__bs_scrMsg">
       <div class="__bs_scrMsg__content">
-        <p class="__bs_scrMsg__descr">Build error</p>
+        <div class="__bs_scrMsg__descr">
+          Build error
+          <button class="__bs_scrMsg__close" type="button" onclick="getElementById('__bs_scrMsg').remove()"><svg class="__bs_scrMsg__close_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs/><path d="M19 6.4L13.4 12l5.6 5.6-1.4 1.4-5.6-5.6L6.4 19 5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5z"/></svg></button>
+        </div>
         <pre class="__bs_scrMsg__code"><%= message %></pre>
       </div>
     </div>
@@ -44,8 +64,8 @@
   const POPUP = document.createElement('div');
   const BODY = document.getElementsByTagName('body')[0];
 
-  socket.on(ERROR_MSG_EVENT, function (error) {
-    let message = error.message.replace(/\[\d+m/gm, '');
+  socket.on(ERROR_MSG_EVENT, (error) => {
+    const message = error.message.replace(/\[\d+m/gm, '');
 
     if (isInPage(POPUP)) {
       BODY.removeChild(POPUP);
