3bc1f7a463349bb6d42587c21675121550f7786f
Yunus Gaziev <yunusga@yandex.ru>
2021-05-26 08:03:01 +0500

    Добавил полифил для NodeList.prototype.forEach, удалил неиспользуемые ф-ции

--- boilerplate/base/js/app.js
+++ boilerplate/base/js/app.js
@@ -1,6 +1,4 @@
-/* ^^^
- * Глобальные-вспомогательные функции
- * ========================================================================== */
+'use strict';
 
 /* ^^^
  * Viewport Height Correction
@@ -8,69 +6,24 @@
  * @link https://www.npmjs.com/package/postcss-viewport-height-correction
  * ========================================================================== */
 function setViewportProperty(){
-  var vh = window.innerHeight * 0.01;
+  let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', vh + 'px');
 }
 window.addEventListener('resize', setViewportProperty);
 setViewportProperty(); // Call the fuction for initialisation
 
 /* ^^^
- * Возвращает HTML-код иконки из SVG-спрайта
+ * Полифил для NodeList.forEach(), на случай если забыл про IE 11
  *
- * @param {String} name Название иконки из спрайта
- * @param {Object} opts Объект настроек для SVG-иконки
- *
- * @example SVG-иконка
- * getSVGSpriteIcon('some-icon', {
- *   tag: 'div',
- *   type: 'icons', // colored для подключения иконки из цветного спрайта
- *   class: '', // дополнительные классы для иконки
- *   mode: 'inline', // external для подключаемых спрайтов
- *   url: '', // путь до файла спрайта, необходим только для подключаемых спрайтов
- * });
- */
-function getSVGSpriteIcon(name, opts) {
-  opts = Object.assign({
-    tag: 'div',
-    type: 'icons',
-    class: '',
-    mode: 'inline',
-    url: '',
-  }, opts);
-
-  let external = '';
-  let typeClass = '';
-
-  if (opts.mode === 'external') {
-    external = `${opts.url}/sprite.${opts.type}.svg`;
-  }
-
-  if (opts.type !== 'icons') {
-    typeClass = ` svg-icon--${opts.type}`;
-  }
-
-  opts.class = opts.class ? ` ${opts.class}` : '';
-
-  return `
-    <${opts.tag} class="svg-icon svg-icon--${name}${typeClass}${opts.class}" aria-hidden="true" focusable="false">
-      <svg class="svg-icon__link">
-        <use xlink:href="${external}#${name}"></use>
-      </svg>
-    </${opts.tag}>
-  `;
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+ * ========================================================================== */
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
 /* ^^^
  * JQUERY Actions
  * ========================================================================== */
 $(function() {
-
-  'use strict';
-
-  /**
-   * определение существования элемента на странице
-   */
-  $.exists = (selector) => $(selector).length > 0;
-
   //=require ../_blocks/**/*.js
 });
