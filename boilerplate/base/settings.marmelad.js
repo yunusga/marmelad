7095389db1e74a75be563a7c99b790811b36ba0e
Yunus Gaziev <yunusga@yandex.ru>
2021-06-29 11:12:38 +0500

    Упростил конфиг для autoprefixer

--- boilerplate/base/settings.marmelad.js
+++ boilerplate/base/settings.marmelad.js
@@ -88,17 +88,7 @@ const iconizer = {
 
 const autoprefixer = {
   overrideBrowserslist: [
-    '>= 1%',
-    'last 2 major version',
-    'not dead',
-    'Chrome >= 45',
-    'Firefox >= 38',
-    'Edge >= 12',
-    'Explorer >= 10',
-    'iOS >= 9',
-    'Safari >= 9',
-    'Android >= 4.4',
-    'Opera >= 30',
+    'last 2 versions',
   ],
 };
 
